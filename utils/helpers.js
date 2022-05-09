a12e7ba3f8a0793c14c70ca8317c1d5d62b6a891
rich p <richardferry3@gmail.com>
2022-05-09 09:17:35 -0700

    add helper to format time and date.

---
+++ utils/helpers.js
@@ -0,0 +1,10 @@
+module.exports = {
+  format_time: (date) => {
+    return date.toLocaleDateString();
+  },
+  format_date: (date) => {
+    return `${new Date(date).getMonth()}/${new Date(date).getDate()}/${
+      new Date(date).getFullYear()
+    }`;
+},
+};
