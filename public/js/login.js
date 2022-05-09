3ea65e2025538bc0cf751af095795484ac8f8676
rich p <richardferry3@gmail.com>
2022-05-09 04:35:44 -0700

    add login and logout javascript functions

---
+++ public/js/login.js
@@ -0,0 +1,24 @@
+async function submitlogin(event){
+event.preventDefault();
+  
+const username = document.querySelector('#username-custom').value.trim();
+const password = document.querySelector('#password-custom').value.trim();
+  
+if (username && password) {
+    const response = await fetch('/api/users/login', {
+    method: 'POST',
+    body: JSON.stringify({ name:username, password:password }),
+    headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+    document.location.replace('/dashboard');
+    console.log('ok')
+    } else {
+    alert('err');
+    }
+}
+};
+  
+const loginBut = document.querySelector('.login-form')
+loginBut.addEventListener('submit', submitlogin);
