f87ae28db5f8a57cf88a2256b616e66a01441848
rich p <richardferry3@gmail.com>
2022-05-08 23:27:15 -0700

    add relationships for Blog, User, and Comment

---
+++ model/index.js
@@ -0,0 +1,28 @@
+const Comment = require('./comment');
+const User = require('./user');
+const Blog = require('./blog');
+
+Blog.belongsTo(User,{
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+Blog.hasMany(Comment,{
+    foreignKey: 'blog_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Blog,{
+    foreignKey: 'blog_id',
+    onDelete: 'CASCADE'
+});
+Comment.belongsTo(User,{
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Blog,{
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+module.exports = {Comment, Blog, User};
