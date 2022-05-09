9af9632e90a58f2ddcfcfc0d6191993af37d9775
rich p <richardferry3@gmail.com>
2022-05-08 21:43:13 -0700

    add function to test if the user is logged into an account present in the database

---
+++ utils/user-auth.js
@@ -0,0 +1,9 @@
+const userAuth = (req, res, next) => {
+    if (!req.session.loggedIn) {
+      res.redirect('user/login');
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = userAuth;
